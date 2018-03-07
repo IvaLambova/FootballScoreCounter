41adce31da52fa2032dd9c03f0d31215f19ef2be
Iva Lambova <iva.lambova@gmail.com>
2018-03-07 14:18:30 +0200

    - Updated the the additional descriptional text of come methods and calls

--- app/src/main/java/com/example/android/footballscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballscorecounter/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.android.footballscorecounter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    // Initiate global integer variables
     int goalTeamA = 0;
     int redCardTeamA = 0;
     int yellowCardTeamA = 0;
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     int redCardTeamB = 0;
     int yellowCardTeamB = 0;
 
-
+    // Initiate global string variables
     static final String stateGoalsTeamA = "teamAGoals";
     static final String stateGoalsTeamB = "teamBGoals";
     static final String stateRedCardsTeamA = "teamARedCards";
@@ -30,22 +30,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Removes the app toolbar to allow for more screen size.
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Hides the app status bar and allows for more screen size
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
     }
 
-    /*
-     * Use onSaveInstanceState(Bundle).
-     */
-
+    // Uses onSaveInstanceState
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
 
-        /*
-         * Calling the superclass so it can save the view hierarchy state.
-         */
+
+        //Calls the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
 
         savedInstanceState.putInt(stateGoalsTeamA, goalTeamA);
@@ -58,61 +55,46 @@ public class MainActivity extends Activity {
 
     }
 
+    // Uses onRestoreInstanceState
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
 
-   /**
-   * Use onRestoreInstanceState.
-   */
+          // Calls the superclass so it can save the view hierarchy state
+          super.onRestoreInstanceState(savedInstanceState);
 
-public void onRestoreInstanceState(Bundle savedInstanceState) {
-
-    /*
-     * Calling the superclass so it can save the view hierarchy state.
-     */
-    super.onRestoreInstanceState(savedInstanceState);
-
-    /**
-    *Saves the user current game state.
-    */
-    goalTeamA = savedInstanceState.getInt(stateGoalsTeamA);
-    goalTeamB = savedInstanceState.getInt(stateGoalsTeamB);
-    redCardTeamA = savedInstanceState.getInt(stateRedCardsTeamA);
-    redCardTeamB = savedInstanceState.getInt(stateRedCardsTeamB);
-    yellowCardTeamA = savedInstanceState.getInt(stateYellowCardsTeamA);
-    yellowCardTeamB = savedInstanceState.getInt(stateYellowCardsTeamB);
+         // Saves the user current game state.
+        goalTeamA = savedInstanceState.getInt(stateGoalsTeamA);
+        goalTeamB = savedInstanceState.getInt(stateGoalsTeamB);
+        redCardTeamA = savedInstanceState.getInt(stateRedCardsTeamA);
+        redCardTeamB = savedInstanceState.getInt(stateRedCardsTeamB);
+        yellowCardTeamA = savedInstanceState.getInt(stateYellowCardsTeamA);
+        yellowCardTeamB = savedInstanceState.getInt(stateYellowCardsTeamB);
 
 
-    /**
-    * Restores state members from saved instance.
-    */
-    displayForTeamA(goalTeamA);
-    displayRedCardsForTeamA(redCardTeamA);
-    displayYellowCardsForTeamA(yellowCardTeamA);
-    displayForTeamB(goalTeamB);
-    displayRedCardsForTeamB(redCardTeamB);
-    displayYellowCardsForTeamB(yellowCardTeamB);
-}
+        //Restores state members from saved instance
+        displayForTeamA(goalTeamA);
+        displayRedCardsForTeamA(redCardTeamA);
+        displayYellowCardsForTeamA(yellowCardTeamA);
+        displayForTeamB(goalTeamB);
+        displayRedCardsForTeamB(redCardTeamB);
+        displayYellowCardsForTeamB(yellowCardTeamB);
+    }
 
 
 
-    /**
-     * Displays +1 goal for Team A when the button GOAL is clicked.
-     */
+
+    // Displays +1 goal for Team A when the button GOAL for Team A is clicked
     public void goalForTeamA(View view) {
         goalTeamA = goalTeamA + 1;
         displayForTeamA(goalTeamA);
     }
 
-    /**
-     * Displays +1 Red Card for Team A when the button Red Card is clicked.
-     */
+    //Displays +1 Red Card for Team A when the button Red Card for Team A is clicked
     public void redCardTeamA (View view) {
         redCardTeamA = redCardTeamA + 1;
         displayRedCardsForTeamA(redCardTeamA);
     }
 
-    /**
-     * Displays +1 Yellow Card for Team A when the button Yellow Card is clicked.
-     */
+    //Displays +1 Yellow Card for Team A when the button Yellow Card for Team A is clicked
     public void yellowCardTeamA (View view) {
         yellowCardTeamA = yellowCardTeamA + 1;
         displayYellowCardsForTeamA(yellowCardTeamA);
@@ -126,75 +108,56 @@ public void onRestoreInstanceState(Bundle savedInstanceState) {
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given number of red cards for Team A.
-     */
+    //Displays the given number of red cards for Team A
     public void displayRedCardsForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.number_red_cards_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given number of yellow cards for Team A.
-     */
+    //Displays the given number of yellow cards for Team A
     public void displayYellowCardsForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.number_yellow_cards_team_a);
         scoreView.setText(String.valueOf(score));
     }
 
 
-    /**
-     * Displays +1 goal for Team B when the button Goal is clicked.
-     */
+    //Displays +1 goal for Team B when the button Goal for Team B is clicked
     public void goalForTeamB (View view) {
         goalTeamB = goalTeamB + 1;
         displayForTeamB(goalTeamB);
     }
 
-    /**
-     * Displays +1 Red Card for Team B when the button Red Card is clicked.
-     */
+    //Displays +1 Red Card for Team B when the button Red Card for Team B is clicked
     public void redCardTeamB (View view) {
         redCardTeamB = redCardTeamB + 1;
         displayRedCardsForTeamB(redCardTeamB);
     }
 
-    /**
-     * Displays +1 Yellow Card for Team B when the button Yellow Card is clicked.
-     */
+    //Displays +1 Yellow Card for Team B when the button Yellow Card for Team B is clicked
     public void yellowCardTeamB (View view) {
         yellowCardTeamB = yellowCardTeamB + 1;
         displayYellowCardsForTeamB(yellowCardTeamB);
     }
 
-    /**
-     * Displays the given score for Team B.
-     */
+    //Displays the given score for Team B
     public void displayForTeamB (int score) {
         TextView scoreView = (TextView) findViewById(R.id.score_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given number of red cards for Team B.
-     */
+    //Displays the given number of red cards for Team B
     public void displayRedCardsForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.number_red_cards_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given number of yellow cards for Team A.
-     */
+    //Displays the given number of yellow cards for Team B
     public void displayYellowCardsForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.number_yellow_cards_team_b);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Resets all numbers (inc. goals, red and yellow cards).
-     */
-
+    //Resets all numbers (inc. goals, red and yellow cards)
     public void resetScore (View v) {
         goalTeamA = 0;
         goalTeamB =0;
